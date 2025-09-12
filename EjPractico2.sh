2ae0db795466f8198d2d4873f09edc42e00d5182
stefanodg07 <cdiazg@fi.uba.ar>
2025-09-11 21:08:10 -0300

    script automatico

--- EjPractico2.sh
+++ EjPractico2.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-touch app.py 
-python3 -m venv ./venv
+
 mkdir TP2-IDS
 cd TP2-IDS
+touch app.py 
 mkdir static templates
 cd static
 mkdir css images
-cd "$HOME/TP2-IDS"
+cd ..
+python3 -m venv .venv
+
+source TP2-IDS/.venv/bin/activate   
+pip install flask
